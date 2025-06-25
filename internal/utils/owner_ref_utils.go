3302f5e371948196457e08926536d4d289a69271
Joey <569475269@qq.com>
2025-06-25 19:32:34 +0800

    fix: new scheduler bugs

--- internal/utils/owner_ref_utils.go
+++ internal/utils/owner_ref_utils.go
@@ -79,6 +79,9 @@ func FindRootOwnerReference(ctx context.Context, c client.Client, namespace stri
 func FindFirstLevelOwnerReference(obj metav1.Object) *metav1.OwnerReference {
 	owners := obj.GetOwnerReferences()
 	if len(owners) == 0 {
+		if obj.GetUID() == "" {
+			return nil
+		}
 		return &metav1.OwnerReference{
 			APIVersion: "v1",
 			Kind:       "Pod",
@@ -88,5 +91,8 @@ func FindFirstLevelOwnerReference(obj metav1.Object) *metav1.OwnerReference {
 		}
 	}
 	ownerRef := owners[0]
+	if ownerRef.UID == "" {
+		return nil
+	}
 	return &ownerRef
 }
