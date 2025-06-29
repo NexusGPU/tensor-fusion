fc82cb53551a31303ff040f0b49afc1bedae3316
knave <shijian104@qq.com>
2025-06-29 09:58:59 +0800

    feat: add auto-scaling switch config parsing and apply, TargetResource support value all

--- internal/autoscaler/workloadstate_test.go
+++ internal/autoscaler/workloadstate_test.go
@@ -83,6 +83,13 @@ var _ = Describe("Workload State", func() {
 		Expect(ws.IsTargetResource("vram")).To(BeTrue())
 
 		ws.AutoScalingConfig = tfv1.AutoScalingConfig{
+			AutoSetResources: tfv1.AutoSetResources{TargetResource: "all"},
+		}
+
+		Expect(ws.IsTargetResource("tflops")).To(BeTrue())
+		Expect(ws.IsTargetResource("vram")).To(BeTrue())
+
+		ws.AutoScalingConfig = tfv1.AutoScalingConfig{
 			AutoSetResources: tfv1.AutoSetResources{TargetResource: "tflops"},
 		}
 		Expect(ws.IsTargetResource("tflops")).To(BeTrue())
@@ -93,5 +100,18 @@ var _ = Describe("Workload State", func() {
 		}
 		Expect(ws.IsTargetResource("tflops")).To(BeFalse())
 		Expect(ws.IsTargetResource("vram")).To(BeTrue())
+	})
+
+	It("should correctly determine if auto scaling is enabled based on config", func() {
+		ws := NewWorkloadState("test")
+
+		ws.AutoScalingConfig = tfv1.AutoScalingConfig{
+			AutoSetResources: tfv1.AutoSetResources{Enable: true},
+		}
+		Expect(ws.IsAutoScalingEnabled()).To(BeTrue())
+		ws.AutoScalingConfig = tfv1.AutoScalingConfig{
+			AutoSetResources: tfv1.AutoSetResources{Enable: false},
+		}
+		Expect(ws.IsAutoScalingEnabled()).To(BeFalse())
 	})
 })
