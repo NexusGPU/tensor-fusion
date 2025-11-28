edfb39387cc90bde67a41aa75190d5c9a7f70db6
code2life <code2life@ustc.edu>
2025-11-28 17:49:24 +0800

    fix: hypervisor name mismatch and test case issue

--- internal/controller/gpunode_controller_test.go
+++ internal/controller/gpunode_controller_test.go
@@ -36,7 +36,7 @@ import (
 
 var _ = Describe("GPUNode Controller", func() {
 	Context("When reconciling gpunodes", func() {
-		It("should create the node discovery job and the hypervisor pod", func() {
+		It("should create the hypervisor pod", func() {
 			tfEnv := NewTensorFusionEnvBuilder().
 				AddPoolWithNodeCount(1).
 				SetGpuCountPerNode(1).
@@ -47,7 +47,7 @@ var _ = Describe("GPUNode Controller", func() {
 			pod := &corev1.Pod{}
 			Eventually(func(g Gomega) {
 				err := k8sClient.Get(ctx, types.NamespacedName{
-					Name:      fmt.Sprintf("hypervisor-%s", gpuNode.Name),
+					Name:      fmt.Sprintf("tf-hypervisor-%s", gpuNode.Name),
 					Namespace: utils.CurrentNamespace(),
 				}, pod)
 				g.Expect(err).ShouldNot(HaveOccurred())
@@ -66,7 +66,7 @@ var _ = Describe("GPUNode Controller", func() {
 			Eventually(func(g Gomega) {
 				newPod := &corev1.Pod{}
 				err := k8sClient.Get(ctx, types.NamespacedName{
-					Name:      fmt.Sprintf("hypervisor-%s", gpuNode.Name),
+					Name:      fmt.Sprintf("tf-hypervisor-%s", gpuNode.Name),
 					Namespace: utils.CurrentNamespace(),
 				}, newPod)
 				g.Expect(err).ShouldNot(HaveOccurred())
