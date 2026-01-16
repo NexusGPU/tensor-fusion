4c0e20712007e9a4138b88d338b356c409d69210
code2life <code2life@ustc.edu>
2026-01-16 13:51:00 +0800

    fix: missing allocator issue

--- internal/autoscaler/autoscaler_suite_test.go
+++ internal/autoscaler/autoscaler_suite_test.go
@@ -35,6 +35,7 @@ import (
 	"github.com/NexusGPU/tensor-fusion/internal/constants"
 	"github.com/NexusGPU/tensor-fusion/internal/controller"
 	"github.com/NexusGPU/tensor-fusion/internal/gpuallocator"
+	"github.com/NexusGPU/tensor-fusion/internal/indexallocator"
 	"github.com/NexusGPU/tensor-fusion/internal/metrics"
 	"github.com/NexusGPU/tensor-fusion/internal/portallocator"
 	"github.com/NexusGPU/tensor-fusion/internal/utils"
@@ -210,11 +211,14 @@ var _ = BeforeSuite(func() {
 	}).SetupWithManager(mgr)
 	Expect(err).ToNot(HaveOccurred())
 
+	indexAllocator, err := indexallocator.NewIndexAllocator(ctx, mgr.GetClient())
+	Expect(err).ToNot(HaveOccurred())
 	err = (&controller.PodReconciler{
-		Client:        mgr.GetClient(),
-		Scheme:        mgr.GetScheme(),
-		Allocator:     allocator,
-		PortAllocator: portAllocator,
+		Client:         mgr.GetClient(),
+		Scheme:         mgr.GetScheme(),
+		Allocator:      allocator,
+		PortAllocator:  portAllocator,
+		IndexAllocator: indexAllocator,
 	}).SetupWithManager(mgr)
 	Expect(err).ToNot(HaveOccurred())
 
